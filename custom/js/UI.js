36c1c9c4d63bc2ba3ad63de7dbbc11be3ad07a1f
chris097 <chrisfidel.international@gmail.com>
2020-08-15 10:14:43 +0100

    fix Bugs

---
+++ custom/js/UI.js
@@ -0,0 +1,14 @@
+export const dropDown = (a, c, d) => {
+    a.forEach((b) => {
+        b.addEventListener('click', () => {
+            c.style.display=d;
+        })
+    })
+}
+
+
+
+
+
+// console.log(checked)
+
---
+++ custom/js/ctrl.js
@@ -0,0 +1,16 @@
+import {dropDown} from './UI.js';
+// import {markComplete} from './UI.js';
+
+//UI classes
+let layer =  document.querySelector('.layer-2'),
+    cards = document.querySelectorAll('.card-1')
+
+
+// call functio
+dropDown(cards, layer, 'block')
+
+// const open2 = document.querySelector('.open-menu')
+// console.log(open2)
+// open2.style.display='block'
+
+
