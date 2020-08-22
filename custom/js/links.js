9b867ac9ed1536c93dabb1eeaa6131f5c166e7c4
chris097 <chrisfidel.international@gmail.com>
2020-08-22 04:08:38 +0100

    added new files: authentication pages, notes and fixes bugs

--- custom/js/links.js
+++ custom/js/links.js
@@ -33,10 +33,15 @@ export function eventController (UIClass){
         UIController(inputBtn.folderLinkContainer, inputBtn.block); 
         UIController(inputBtn.popUp, inputBtn.block)
     }
+    const removeDeleteTask = () => {
+        UIController(inputBtn.deleteTask, inputBtn.none); 
+        UIController(inputBtn.popUp, inputBtn.none)
+    }
 
     UIController(inputBtn.open2).addEventListener(inputBtn.click, displayLinks);
     UIController(inputBtn.close2).addEventListener(inputBtn.click, removeLinks);
     UIController(inputBtn.newBtn).addEventListener(inputBtn.click, displayAddLink);
     UIController(inputBtn.createTaskExit).addEventListener(inputBtn.click, removeAddLink);
     UIController(inputBtn.btnFolder).addEventListener(inputBtn.click, folderLink);
+    UIController(inputBtn.deleteTaskExit).addEventListener(inputBtn.click, removeDeleteTask);
 }
